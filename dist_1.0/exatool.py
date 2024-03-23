57f4a4cd0f7f7a263d1f1ff8d661b9ae79b3a0f1
Matteo_btti <151944715+ZeepReactorr@users.noreply.github.com>
2024-03-23 18:04:55 +0100

    Update exatool.py

--- dist_1.0/exatool.py
+++ dist_1.0/exatool.py
@@ -206,7 +206,7 @@ def switch_page(url, pure_url):
     client.close()
     db = soup(htmldata, "html.parser")
     locator = db.findAll('span', {'class':'value'})  
-    nb_articles = re.findall('[0-9]+', str(locator[0].strip(','))
+    nb_articles = re.findall('[0-9]+', str(locator[0].strip(',')))
     limite = (int(nb_articles[0])//10)+1
     if limite > 100:
         limite = 100
